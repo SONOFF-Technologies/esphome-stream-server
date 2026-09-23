13bb8d3a6c2c0d08fb0e00076beb064b93b146e9
SuiKa <44567811+SuiKaSan@users.noreply.github.com>
2026-09-23 10:11:16 +0800

    Fix build on ESPHome 2026.7+ and the parse_esphome_version() deprecation

--- components/stream_server/__init__.py
+++ components/stream_server/__init__.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID, CONF_PORT, CONF_BUFFER_SIZE
-from esphome.util import parse_esphome_version
 
 # ESPHome doesn't know the Stream abstraction yet, so hardcode to use a UART for now.
 
@@ -46,6 +45,5 @@ async def to_code(config):
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
 
-    esphome_version = parse_esphome_version()
-    if (2025, 12, 0) <= esphome_version < (2026, 3, 0):
+    if hasattr(uart, "request_wake_loop_on_rx"):
         uart.request_wake_loop_on_rx()
